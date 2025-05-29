d3d4977f6a0e262d87cc4e309c3be0454db7f2be
Huzzi <huzzi@computer>
2025-05-29 22:29:26 +0200

    Header work, and hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
 import { Link } from 'react-router-dom';  // 🆕 Import de Link
+import  { useState, useEffect } from 'react';
+
+
 
 function Header() {
+  const [isShrunk, setIsShrunk] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsShrunk(true);
+      } else {
+        setIsShrunk(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header className="header">
-      <div className="logo">AurelienJ</div>
+    <header className={`header ${isShrunk ? 'shrink' : ''}`}>
+      <div className="logo">aurelienj.</div>
       <nav className="nav">
-        <Link to="/">Accueil</Link>
+        <Link to="/">Work</Link>
         <Link to="/services">Services</Link>
         <Link to="/contact">Contact</Link>
       </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+
 function Home() {
+  const handleScroll = () => {
+    const target = document.getElementById("MyWork");
+    if (target) {
+      const top = target.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({
+        top: top - 60, // Décalage personnalisé
+        behavior: "smooth"
+      });
+    }
+  };
+
   return (
-    <div>
-      <h1>Bienvenue sur mon site</h1>
-      <p>Voici mon travail...</p>
-    </div>
-  )
+    <>
+      <div className="heroSection">
+        <div className="heroTxt">
+          <p>I’m Aurelien, a freelancer specializing in creating fully responsive websites for artisans. I work with craftsmen and small businesses, offering complete digital solutions, from showcase websites to e-commerce platforms.</p>
+          <button className='heroBtn' onClick={handleScroll}>
+            See My Work
+          </button>
+        </div>
+      </div>
+
+      <div
+        id="MyWork"
+        className="MyWork"
+        style={{ height: "100vh", backgroundColor: "lightblue" }}
+      >
+        <h2>Mon Travail</h2>
+      </div>
+    </>
+  );
 }
-export default Home
+
+export default Home;
