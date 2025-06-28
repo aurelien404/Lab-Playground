216241f3d9beaa5232f6fcbe988fb90fdb4113b5
aurelienj <contact@aurelienj.ch>
2025-06-28 11:27:44 +0200

    Initial commit: frontend and backend setup

---
+++ frontend/OpenTasker/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
